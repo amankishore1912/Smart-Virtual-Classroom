56ba54dae90b04f81e9f3bd9e1e6a5880f53c90b
Aman Kishore <112801004+amankishore1912@users.noreply.github.com>
2024-03-14 19:05:55 +0530

    Add files via upload

---
+++ EduSphere-Live-Class (Main)/README.md
@@ -0,0 +1,9 @@
+# Installation
+* 1 - clone repo 
+* 2 - Create an account on agora.io and create an app to generate an APP ID
+* 3 - Update APP ID, Temp Token and Channel Name in room_rtc.js
+```javascript
+let APP_ID = "YOU-APP-ID"
+```
+
+  
---
+++ EduSphere-Live-Class (Main)/js/room.js
@@ -0,0 +1,88 @@
+let messagesContainer = document.getElementById('messages');
+messagesContainer.scrollTop = messagesContainer.scrollHeight;
+
+const memberContainer = document.getElementById('members__container');
+const memberButton = document.getElementById('members__button');
+
+const chatContainer = document.getElementById('messages__container');
+const chatButton = document.getElementById('chat__button');
+
+let activeMemberContainer = false;
+
+function openLinkAsPopup() {
+  // URL of the link
+  var url = "http://127.0.0.1:5000/";
+  // Popup window properties
+  var popupWidth = 600;
+  var popupHeight = 400;
+  var popupLeft = (window.screen.width - popupWidth) / 2;
+  var popupTop = (window.screen.height - popupHeight) / 2;
+  // Open the link in a popup window
+  window.open(url, 'Popup', 'width=' + popupWidth + ', height=' + popupHeight + ', left=' + popupLeft + ', top=' + popupTop);
+}
+
+memberButton.addEventListener('click', () => {
+  if (activeMemberContainer) {
+    memberContainer.style.display = 'none';
+  } else {
+    memberContainer.style.display = 'block';
+  }
+
+  activeMemberContainer = !activeMemberContainer;
+});
+
+let activeChatContainer = false;
+
+chatButton.addEventListener('click', () => {
+  if (activeChatContainer) {
+    chatContainer.style.display = 'none';
+  } else {
+    chatContainer.style.display = 'block';
+  }
+
+  activeChatContainer = !activeChatContainer;
+});
+
+let displayFrame = document.getElementById('stream__box')
+let videoFrames = document.getElementsByClassName('video__container')
+let userIdInDisplayFrame = null;
+
+let expandVideoFrame = (e) => {
+
+  let child = displayFrame.children[0]
+  if(child){
+      document.getElementById('streams__container').appendChild(child)
+  }
+
+  displayFrame.style.display = 'block'
+  displayFrame.appendChild(e.currentTarget)
+  userIdInDisplayFrame = e.currentTarget.id
+
+  for(let i = 0; videoFrames.length > i; i++){
+    if(videoFrames[i].id != userIdInDisplayFrame){
+      videoFrames[i].style.height = '100px'
+      videoFrames[i].style.width = '100px'
+    }
+  }
+
+}
+
+for(let i = 0; videoFrames.length > i; i++){
+  videoFrames[i].addEventListener('click', expandVideoFrame)
+}
+
+
+let hideDisplayFrame = () => {
+    userIdInDisplayFrame = null
+    displayFrame.style.display = null
+
+    let child = displayFrame.children[0]
+    document.getElementById('streams__container').appendChild(child)
+
+    for(let i = 0; videoFrames.length > i; i++){
+      videoFrames[i].style.height = '300px'
+      videoFrames[i].style.width = '300px'
+  }
+}
+
+displayFrame.addEventListener('click', hideDisplayFrame)
